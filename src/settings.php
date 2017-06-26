21ddc555ee8be86067f90cd289895c84e142885f
Sugar <hxtgirq710@qq.com>
2017-06-26 11:06:24 +0800

    配置blade

--- src/settings.php
+++ src/settings.php
@@ -4,9 +4,10 @@ return [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
+        // blade settings
+        'blade' => [
+            'view_path' => __DIR__ . '/../templates/',
+            'cache_path' => __DIR__ . '/../caches/templates/'
         ],
 
         // Monolog settings

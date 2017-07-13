5c74774ffc5746aec72c4cff7996a2694ee89c54
Sugar <hxtgirq710@qq.com>
2017-07-13 09:23:25 +0800

    修改页面中文件提示

--- templates/adm/auth/login.blade.php
+++ templates/adm/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Log in</title>
+    <title>SlimLTE | 登录</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>Slim</b>LTE</a>
+        <a href="{{ url('/admin') }}"><b>Slim</b>LTE</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
@@ -37,18 +37,18 @@
         @endif
         <form action="{{ url('/admin/login') }}" method="post">
             <div class="form-group has-feedback">
-                <input type="text" class="form-control" name="username" placeholder="Email or Username">
+                <input type="text" class="form-control" name="username" placeholder="账号或邮箱">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" name="password" placeholder="Password">
+                <input type="password" class="form-control" name="password" placeholder="登录密码">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox" name="remember" value="T"> Remember Me
+                            <input type="checkbox" name="remember" value="T"> 保持登录
                         </label>
                     </div>
                 </div>

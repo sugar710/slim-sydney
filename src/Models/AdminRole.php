549d432d6caea9044d34feb476a21660569aa6cc
冯毅 <hxtgirq710@qq.com>
2017-08-13 00:45:07 +0800

    角色管理 （未测)

---
+++ src/Models/AdminRole.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * 角色管理
+ *
+ * Class AdminRole
+ * @package App\Models
+ */
+class AdminRole extends Model {
+
+    protected $table = "admin_role";
+    protected $guarded = [];
+
+}

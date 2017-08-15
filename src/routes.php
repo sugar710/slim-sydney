74760b36b61d008e3b5242ccb8b0e1ee0660957f
Sugar <hxtgirq710@qq.com>
2017-08-15 10:59:44 +0800

    独立安装路由

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,6 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Controllers\Admin\AuthController;
 use App\Controllers\Admin\HomeController;
-use App\Controllers\InstallController;
 use App\Controllers\Admin\AdminPermissionController;
 use App\Controllers\Admin\AdminRoleController;
 use App\Middleware\VerifyAdminLoginMiddleware;
@@ -14,6 +13,10 @@ $app->get('/admin/login', AuthController::class . ':login');
 $app->post('/admin/login', AuthController::class . ':doLogin');
 
 $app->get('/admin/logout', AuthController::class . ':logout');
+
+$app->get('/admin', function(Request $req, Response $res) {
+    return $res->withRedirect("/admin/home", 301);
+});
 
 $app->group("/admin", function() use ($app) {
     $app->get("/home", HomeController::class . ':home');
@@ -32,30 +35,4 @@ $app->group("/admin", function() use ($app) {
 
 })->add(VerifyAdminLoginMiddleware::class);
 
-$app->get("/install", function(Request $req, Response $res) {
-    return $res->withRedirect("/install/welcome");
-});
-
-$app->group("/install", function() use ($app){
-
-    $app->get("/", function(Request $req, Response $res){
-        return $res->withRedirect('/install/welcome');
-    });
-
-   $app->get("/welcome", InstallController::class . ':welcome');
-
-   $app->post("/agree/verify", InstallController::class . ':agreeVerify');
-
-   $app->get("/env", InstallController::class . ':env');
-
-   $app->get("/database", InstallController::class . ':database');
-
-   $app->post("/database/verify", InstallController::class . ':databaseVerify');
-
-   $app->get("/account", InstallController::class . ':accountConf');
-
-   $app->post("/account/verify", InstallController::class . ":accountVerify");
-
-   $app->get("/ready/go", InstallController::class . ':doInstall');
-
-});
+require __DIR__ . '/Routers/install.php';

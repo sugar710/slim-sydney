2ab05a82b9a3cdcd7078e26b2a3315734a80ebd0
Sugar <hxtgirq710@qq.com>
2017-09-01 15:11:52 +0800

    程序安装时初始化数据

--- doc/slimLte.sql
+++ doc/slimLte.sql
@@ -1,14 +1,14 @@
-
 -- -----------------------------------------------------
--- Table `sydney`.`admin_user`
+-- Table `admin_user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_user` (
+CREATE TABLE IF NOT EXISTS `admin_user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL COMMENT '用户名称',
   `avatar` VARCHAR(50) NOT NULL COMMENT '头像地址',
   `username` VARCHAR(45) NOT NULL COMMENT '登录账号',
   `password` VARCHAR(60) NOT NULL COMMENT '用户密码',
   `email` VARCHAR(200) NOT NULL COMMENT '邮箱',
+  `is_lock` ENUM('T', 'F') NOT NULL DEFAULT 'F' COMMENT '是否锁定',
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   `deleted_at` DATETIME NULL,
@@ -18,9 +18,9 @@ COMMENT = '用户';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_role`
+-- Table `admin_role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_role` (
+CREATE TABLE IF NOT EXISTS `admin_role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `slug` VARCHAR(50) NOT NULL,
@@ -33,24 +33,22 @@ COMMENT = '角色';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_user_role`
+-- Table `admin_user_role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_user_role` (
+CREATE TABLE IF NOT EXISTS `admin_user_role` (
   `id` INT NULL AUTO_INCREMENT,
-  `admin_role_id` INT NOT NULL,
-  `admin_user_id` INT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `role_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 COMMENT = '用户角色关联表';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_router`
+-- Table `admin_router`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_router` (
-  `id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `admin_router` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL COMMENT '路由名称',
   `path` VARCHAR(80) NOT NULL COMMENT '路由地址',
   `query` VARCHAR(100) NOT NULL COMMENT '附加参数',
@@ -67,13 +65,14 @@ COMMENT = '管理后台路由表';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_menu`
+-- Table `admin_menu`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_menu` (
+CREATE TABLE IF NOT EXISTS `admin_menu` (
   `id` INT NULL AUTO_INCREMENT,
   `pid` INT NOT NULL DEFAULT 0,
   `name` VARCHAR(45) NOT NULL COMMENT '菜单名称',
   `router_id` INT UNSIGNED NOT NULL,
+  `sort` INT NOT NULL DEFAULT 500,
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   `deleted_at` DATETIME NULL,
@@ -83,9 +82,9 @@ COMMENT = '菜单管理';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`setting`
+-- Table `setting`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`setting` (
+CREATE TABLE IF NOT EXISTS `setting` (
   `id` INT NULL AUTO_INCREMENT,
   `key` VARCHAR(50) NOT NULL,
   `value` VARCHAR(150) NOT NULL,
@@ -97,9 +96,9 @@ COMMENT = '系统配置';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`category`
+-- Table `category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`category` (
+CREATE TABLE IF NOT EXISTS `category` (
   `id` INT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL COMMENT '分类名称',
   `slug` VARCHAR(45) NOT NULL COMMENT '分类标记',
@@ -111,9 +110,9 @@ COMMENT = '分类';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`article`
+-- Table `article`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`article` (
+CREATE TABLE IF NOT EXISTS `article` (
   `id` INT NULL AUTO_INCREMENT,
   `category_id` INT NOT NULL COMMENT '文章分类ID',
   `title` VARCHAR(150) NOT NULL COMMENT '文章标题',
@@ -129,9 +128,9 @@ COMMENT = '文章表';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`tags`
+-- Table `tags`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`tags` (
+CREATE TABLE IF NOT EXISTS `tags` (
   `id` INT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL COMMENT '标签名称',
   `use_cnt` INT NOT NULL DEFAULT 0 COMMENT '使用数量',
@@ -143,9 +142,9 @@ COMMENT = '标签';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`article_tag`
+-- Table `article_tag`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`article_tag` (
+CREATE TABLE IF NOT EXISTS `article_tag` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `tag_id` INT NOT NULL COMMENT '标签ID',
   `article_id` INT NOT NULL COMMENT '文章ID',
@@ -157,9 +156,9 @@ COMMENT = '文章标签关联表';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`comment`
+-- Table `comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`comment` (
+CREATE TABLE IF NOT EXISTS `comment` (
   `id` INT NULL AUTO_INCREMENT,
   `article_id` INT NOT NULL COMMENT '文章ID',
   `content` VARCHAR(150) NOT NULL COMMENT '评论内容',
@@ -172,23 +171,40 @@ COMMENT = '评论';
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_role_router`
+-- Table `admin_role_router`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_role_router` (
+CREATE TABLE IF NOT EXISTS `admin_role_router` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `router_id` INT UNSIGNED NOT NULL,
   `role_id` INT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sydney`.`admin_user_router`
+-- Table `admin_user_router`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sydney`.`admin_user_router` (
+CREATE TABLE IF NOT EXISTS `admin_user_router` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `router_id` INT UNSIGNED NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `operation_log`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `operation_log` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL DEFAULT 0,
+  `method` VARCHAR(45) NOT NULL DEFAULT 'GET',
+  `path` VARCHAR(45) NOT NULL DEFAULT '/',
+  `ip` VARCHAR(15) NOT NULL DEFAULT '0.0.0.0',
+  `input` TEXT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+COMMENT = '操作日志';

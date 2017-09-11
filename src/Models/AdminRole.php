66bd9a5954f3ff3b4a36e9ce8ef2d03fe17f73f7
Sugar <hxtgirq710@qq.com>
2017-09-11 15:23:21 +0800

    角色路由关联处理

--- src/Models/AdminRole.php
+++ src/Models/AdminRole.php
@@ -8,9 +8,15 @@ namespace App\Models;
  * Class AdminRole
  * @package App\Models
  */
-class AdminRole extends Model {
+class AdminRole extends Model
+{
 
     protected $table = "admin_role";
     protected $guarded = [];
+
+    public function routers()
+    {
+        return $this->belongsToMany(AdminRouter::class, 'admin_role_router', 'role_id', 'router_id');
+    }
 
 }

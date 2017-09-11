81b871d7d9ae90b5898fd3e84669ec8ca7eb0fdd
Sugar <hxtgirq710@qq.com>
2017-09-11 16:33:47 +0800

    路由&角色 权限验证中间件

--- src/Models/User.php
+++ src/Models/User.php
@@ -32,5 +32,35 @@ class User extends Model
         return $this->belongsToMany(AdminRouter::class, 'admin_user_router', 'user_id', 'router_id');
     }
 
+    /**
+     * 用户是否含用某角色
+     *
+     * @param $role
+     * @return bool
+     */
+    public function isRole($role) {
+        $slugs = $this->roles()->pluck("slug")->toArray();
+        return in_array($role, $slugs);
+    }
+
+    /**
+     * 当前用户是否有调用该路由的权限
+     *
+     * @param AdminRouter $router
+     * @return bool
+     */
+    public function hasRouter(AdminRouter $router) {
+        $routers = $this->routers()->pluck("router_id")->toArray();
+        if (in_array($router->id, $routers)) {
+            return true;
+        }
+        foreach($this->roles as $role) {
+            if($role->hasRouter($router)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
